207f01c92c1e0efe9633260178ff90b6f1813230
赖海斌 <12211612@mail.sustech.edu.cn>
2023-03-03 22:46:34 +0800

    Initial commit 2023/3/3 尝试写一个登录秀米的程序

---
+++ OnWeb.py
@@ -0,0 +1,43 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import Document
+
+input()
+
+url = 'https://xiumi.us/auth#/'
+#秀米的网址
+
+EMailName = "sdc@sustc.edu.cn"
+Password  = "shudenumber1"
+#树德书院的邮箱和密码
+
+driver = webdriver.Chrome()
+
+driver.get(url)#访问
+driver.maximize_window()#大屏幕
+
+time.sleep(0.5)#休息庆祝一下
+
+login = driver.find_element(By.NAME, 'email').send_keys(EMailName)
+passwordCode = driver.find_element(By.NAME, 'password').send_keys(Password)#填写账号密码
+
+time.sleep(0.5)
+button = driver.find_element(By.XPATH, "/html/body/div[1]/section/div/div[1]/form/div[4]/button").click()
+#进入！
+
+
+time.sleep(0.5)
+button2 = driver.find_element(By.XPATH, '//*[@id="tn_xiumi_header_navbar_nav"]/ul/li[1]/a').click()
+
+time.sleep(0.5)
+
+url2 = 'https://xiumi.us/studio/v5#/paper'
+driver.get(url2)
+
+time.sleep(1)
+
+path = "/Input.docx"
+
+input()
